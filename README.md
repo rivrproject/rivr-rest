04de999b04b8d24c963ab2558b05344f7afb5110
Kyle Fuller <inbox@kylefuller.co.uk>
2015-02-22 14:38:28 +0000

    Inital project

---
+++ README.md
@@ -0,0 +1,9 @@
+# rivr-rest
+
+[![Build Status](http://img.shields.io/travis/rivrproject/rivr-rest/master.svg?style=flat)](https://travis-ci.org/rivrproject/rivr-rest)
+
+**Work in Progress**: *This repository is not yet ready for consumption.*
+
+## License
+
+rivr-rest is released under the BSD license. See [LICENSE](LICENSE).
---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='rivr-rest',
+    version='0.1.0',
+    description='rivr integration to make building REST APIs easy.',
+    url='https://github.com/rivrproject/rivr-rest',
+    packages=find_packages(),
+    install_requires=[
+        'rivr',
+    ],
+    author='Kyle Fuller',
+    author_email='inbox@kylefuller.co.uk',
+    license='BSD',
+    classifiers=(
+      'Programming Language :: Python :: 2',
+      'Programming Language :: Python :: 2.7',
+      'Programming Language :: Python :: 3',
+      'Programming Language :: Python :: 3.2',
+      'Programming Language :: Python :: 3.3',
+      'Programming Language :: Python :: 3.4',
+      'License :: OSI Approved :: BSD License',
+    )
+)
+
