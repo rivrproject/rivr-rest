0813fa26a4dc2d640d38eeef93e602e4044373b3
Kyle Fuller <kyle@fuller.li>
2015-02-28 01:28:37 +0000

    [Router] Add built in root resource

--- rivr_rest/router.py
+++ rivr_rest/router.py
@@ -1,5 +1,6 @@
 import re
 from rivr.http import Http404
+from rivr_rest.resource import Resource
 
 
 EXTRACT_VARIABLE_REGEX = re.compile(r'\{([\w]+)\}')
@@ -29,12 +30,37 @@ def extract(uri_template, uri):
     return None
 
 
+class RootResource(Resource):
+    uri_template = '/'
+
+    def __init__(self, resources):
+        self.resources = resources
+
+    def get_relations(self):
+        def to_resource(relation):
+            if isinstance(relation[1], type):
+                return (relation[0], relation[1]())
+
+            return relation
+
+        return dict(map(to_resource, self.resources.items()))
+
+    def can_embed(self, relation):
+        return False
+
+
 class Router(object):
+    root_resource = RootResource
+
     def __init__(self, *resources):
+        self.root_resources = {}
         self.resources = list(resources)
 
     def register(self, resource):
         self.resources.append(resource)
+
+    def add_root_resource(self, relation, resource):
+        self.root_resources[relation] = resource
 
     def resolve(self, path):
         for resource in self.resources:
@@ -45,6 +71,10 @@ class Router(object):
         return None
 
     def __call__(self, request):
+        if request.path == '/' and len(self.root_resources) > 0:
+            resource = self.root_resource(resources=self.root_resources)
+            return resource.dispatch(request)
+
         match = self.resolve(request.path)
         if not match:
             raise Http404
